ce4859fee2da691ee28adc97931f30b389f099db
Nariman05Manas <89251464474@mail.ru>
2022-10-14 08:50:33 +0300

    Initial Commit

---
+++ VFollower(diplom project Netology)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VFollower(diplom project Netology)
+//
+//  Created by qwerty on 14.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
